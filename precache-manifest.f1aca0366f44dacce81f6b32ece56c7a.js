39cd01422b91de9c8bcf43034945b9b4352efe57
dmb-2222 <dmb-2222@yandex.ru>
2019-11-24 16:29:11 +0200

    Updates

---
+++ precache-manifest.f1aca0366f44dacce81f6b32ece56c7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20406176f964bab95f0a05b0dfb8adfa",
+    "url": "/goit-react14-hw-04-movies/index.html"
+  },
+  {
+    "revision": "d9bd6b1a1d43352b6a5c",
+    "url": "/goit-react14-hw-04-movies/static/css/main.bd9f783a.chunk.css"
+  },
+  {
+    "revision": "8ac1eb1ec66b262f5c45",
+    "url": "/goit-react14-hw-04-movies/static/js/2.c182e020.chunk.js"
+  },
+  {
+    "revision": "d9bd6b1a1d43352b6a5c",
+    "url": "/goit-react14-hw-04-movies/static/js/main.8596143c.chunk.js"
+  },
+  {
+    "revision": "c33d44705de9cf6b808d",
+    "url": "/goit-react14-hw-04-movies/static/js/runtime-main.b3d7f42f.js"
+  }
+]);
